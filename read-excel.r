dded89f88f27137911577aa289c76d451d3bd7d8
Eric Magar <emagar@gmail.com>
2024-01-24 07:26:15 -0600

    add read excel

---
+++ read-excel.r
@@ -0,0 +1,12 @@
+read.excel <- function(header=TRUE,...) {
+    ## From https://www.r-bloggers.com/2013/02/copying-data-from-excel-to-r-and-back/
+    ## Usage: copy from excel into clipboard then dat <- read.excel()
+    read.table("clipboard",sep="\t",header=header,...)
+}
+
+write.excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
+    ## From https://www.r-bloggers.com/2013/02/copying-data-from-excel-to-r-and-back/
+    ## Usage: write.excel(dat)
+  write.table(x,"clipboard",sep="\t",row.names=row.names,col.names=col.names,...)
+}
+ 
