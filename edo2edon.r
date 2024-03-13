0b645194ef6fffea000b7929a280b85b2e849049
Eric Magar <emagar@gmail.com>
2024-03-13 09:31:45 -0600

    Adds edo2edon.r and read-excel.r

---
+++ edo2edon.r
@@ -0,0 +1,23 @@
+require(plyr)
+##
+edo2edon <- function(x){
+    edon <- mapvalues(
+        x,
+        from = c("ags", "bc",  "bcs", "cam", "coa", "col", "cps", "cua", "df",  "dgo", "gua", "gue", "hgo", "jal", "mex", "mic",
+                 "mor", "nay", "nl",  "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac"),
+        to   = 1:32
+    )
+    edon <- as.numeric(edon)
+    return(edon)
+}
+##
+##
+edon2edo <- function(x){
+    edo <- mapvalues(
+        x,
+        from = 1:32,
+        to   = c("ags", "bc",  "bcs", "cam", "coa", "col", "cps", "cua", "df",  "dgo", "gua", "gue", "hgo", "jal", "mex", "mic",
+                 "mor", "nay", "nl",  "oax", "pue", "que", "qui", "san", "sin", "son", "tab", "tam", "tla", "ver", "yuc", "zac")
+    )
+    return(edo)
+}
--- read-excel.r
+++ read-excel.r
@@ -7,6 +7,6 @@ read.excel <- function(header=TRUE,...) {
 write.excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
     ## From https://www.r-bloggers.com/2013/02/copying-data-from-excel-to-r-and-back/
     ## Usage: write.excel(dat)
-  write.table(x,"clipboard",sep="\t",row.names=row.names,col.names=col.names,...)
+    write.table(x,"clipboard",sep="\t",row.names=row.names,col.names=col.names,...)
 }
- 
+
